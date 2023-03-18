2fa7a0cbfa17c90617799586f087378cfeb5258e
Sedanur Çevik <sdnrcvk@gmail.com>
2023-03-18 15:47:58 +0300

    firebase installed

---
+++ firebase/firebase.js
@@ -0,0 +1,17 @@
+import {initializeApp} from 'firebase/app'
+import {getFirebase} from 'firebase/firestore'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyB--are9M5XbH6hSMG5qwKKnxzGQU_6tsk",
+    authDomain: "modern-react-app-57830.firebaseapp.com",
+    projectId: "modern-react-app-57830",
+    storageBucket: "modern-react-app-57830.appspot.com",
+    messagingSenderId: "722629767790",
+    appId: "1:722629767790:web:9ceda586aa9160fa836c7f"
+  };
+
+  initializeApp(firebaseConfig);
+  
+  const db=getFirebase();
+
+  export {db}
