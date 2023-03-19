bcb3c21d9a87fcc639df394e7c0b759faf588aeb
Sedanur Çevik <sdnrcvk@gmail.com>
2023-03-19 19:42:00 +0300

    todo updated on firestore

--- components/TodoForm.js
+++ components/TodoForm.js
@@ -1,5 +1,5 @@
 import { Button,TextField,Typography } from "@mui/material"
-import { collection,addDoc,serverTimestamp } from "firebase/firestore"
+import { collection,addDoc,serverTimestamp,updateDoc,doc } from "firebase/firestore"
 import { db } from "@/firebase/firebase"
 import { async } from "@firebase/util"
 import { useContext,useState,useRef,useEffect } from "react"
@@ -31,22 +31,40 @@ export default function TodoForm() {
             showAlert("error","Başlık ya da açıklama boş geçilemez")
             return;
         }
-        const ref=collection(db,"todos");
-        const docRef=await addDoc(ref,{...todo,tarih:serverTimestamp()});
-        console.log(docRef);
-        setTodo({baslik:"",aciklama:""})
-        //alert(`${docRef.id} id li todo eklendi`)
-        showAlert("success",`${docRef.id} id'li todo eklendi`);
+
+        if(todo?.hasOwnProperty("id")){
+            //güncelleme
+            const ref=doc(db,"todos",todo.id);
+            const newTodo={baslik:todo.baslik,aciklama:todo.aciklama,sonGuncellemeTarih:serverTimestamp()};
+            
+            updateDoc(ref,newTodo);
+            setTodo({baslik:"",aciklama:""})
+            showAlert("success","todo güncellendi")
+        }else{
+            //ekleme
+            const ref=collection(db,"todos");
+            const docRef=await addDoc(ref,{...todo,tarih:serverTimestamp()});
+            console.log(docRef);
+            setTodo({baslik:"",aciklama:""})
+            //alert(`${docRef.id} id li todo eklendi`)
+            showAlert("success",`${docRef.id} id'li todo eklendi`);
+        }
+
     }
 
   return (
     <div ref={inputRef}>
-        <pre>{JSON.stringify(todo,null,"\t")}</pre>
         <Typography sx={{mt:3,fontWeight:"bold"}} variant="h5"
         color="darkgrey">Yeni Todo Ekle</Typography>
         <TextField value={todo.baslik} fullWidth label="Başlık" margin="normal" onChange={(e)=>setTodo({...todo,baslik:e.target.value})}></TextField>
         <TextField value={todo.aciklama} fullWidth label="Açıklama" multiline maxRows={3} margin="normal" onChange={(e)=>setTodo({...todo,aciklama:e.target.value})}></TextField>
-        <Button sx={{mt:3}} variant="outlined" color="success" onClick={handleClick}>Todo Ekle</Button>
+        {
+            todo?.hasOwnProperty("id") ? (
+                <Button sx={{mt:3}} variant="outlined" color="warning" onClick={handleClick}>Todo Güncelle</Button>
+            ) : (
+                <Button sx={{mt:3}} variant="outlined" color="success" onClick={handleClick}>Todo Ekle</Button>
+            )
+        }
     </div>
   )
 }
