04ac2508767e867e92ee99293679d8ffd025fa72
Sedanur Çevik <sdnrcvk@gmail.com>
2023-03-19 15:47:20 +0300

    Todo added to firestore

--- components/TodoForm.js
+++ components/TodoForm.js
@@ -1,13 +1,35 @@
 import { Button,TextField,Typography } from "@mui/material"
+import { useState } from "react"
+import { collection,addDoc,serverTimestamp } from "firebase/firestore"
+import { db } from "@/firebase/firebase"
+import { async } from "@firebase/util"
 
 export default function TodoForm() {
+    const [todo,setTodo]=useState({
+        baslik:"",
+        aciklama:""
+    })
+
+    const handleClick=async(e)=>{
+        e.preventDefault();
+        //console.log(todo);
+        if(todo.baslik=="" || todo.aciklama=="" ){
+            return;
+        }
+        const ref=collection(db,"todos");
+        const docRef=await addDoc(ref,{...todo,tarih:serverTimestamp()});
+        console.log(docRef);
+        setTodo({baslik:"",aciklama:""})
+        alert(`${docRef.id} id li todo eklendi`)
+    }
+
   return (
     <div>
         <Typography sx={{mt:3,fontWeight:"bold"}} variant="h5"
         color="darkgrey">Yeni Todo Ekle</Typography>
-        <TextField fullWidth label="Başlık" margin="normal"></TextField>
-        <TextField fullWidth label="Açıklama" multiline maxRows={3} margin="normal"></TextField>
-        <Button sx={{mt:3}} variant="outlined" color="success">Todo Ekle</Button>
+        <TextField value={todo.baslik} fullWidth label="Başlık" margin="normal" onChange={(e)=>setTodo({...todo,baslik:e.target.value})}></TextField>
+        <TextField value={todo.aciklama} fullWidth label="Açıklama" multiline maxRows={3} margin="normal" onChange={(e)=>setTodo({...todo,aciklama:e.target.value})}></TextField>
+        <Button sx={{mt:3}} variant="outlined" color="success" onClick={handleClick}>Todo Ekle</Button>
     </div>
   )
 }
